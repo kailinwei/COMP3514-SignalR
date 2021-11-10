9cda87cc338c506a73070a31f66f37622f84b95b
kailinwei <kailinwei95@gmail.com>
2021-11-09 19:41:02 -0800

    first commit

---
+++ Hubs/DrawDotHub.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+public class DrawDotHub: Hub {
+   public async Task UpdateCanvas(int x, int y, string color) {
+      await Clients.All.SendAsync("updateDot",x, y, color);
+   }
+
+   public async Task ClearCanvas() {
+      await Clients.All.SendAsync("clearCanvas");
+   }
+}
